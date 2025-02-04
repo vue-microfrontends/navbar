03f80b2609b368399f6d2dc2ad4379f288e0e0eb
Joel Denning <joeldenning@gmail.com>
2025-02-04 15:02:10 -0600

    Upgrade dependencies, switch to native modules (#38)
    * Upgrade dependencies, switch to native modules
    
    * Fix lint
    
    * Remove test script since no tests

--- vue.config.js
+++ vue.config.js
@@ -1,15 +1,7 @@
 module.exports = {
   lintOnSave: false,
   configureWebpack: {
-    devServer: {
-      headers: {
-        "Access-Control-Allow-Origin": "*"
-      },
-      disableHostCheck: true,
-      sockPort: 8501,
-      sockHost: "localhost"
-    },
-    externals: ["vue", "vue-router", /^@vue-mf\/.+/]
+    externals: ["vue", "vue-router", /^@vue-mf\/.+/],
   },
   filenameHashing: false
 };
